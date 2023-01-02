35edda1a79c3f80f20fdc78f13a14b8bc43d22e7
Arasya Rafi <94653668+GetSya@users.noreply.github.com>
2023-01-02 07:45:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'Arasya' 
+global.apikey = ["Jo", "Syaa"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
